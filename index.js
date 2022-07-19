c52333643b841a546e0f4d47a1509fd808d744c8
Sean Oh <seanoh@ucsb.edu>
2022-07-19 10:17:27 -0700

    write index.js stubs

---
+++ index.js
@@ -0,0 +1,54 @@
+// DEPENDENCIES
+const inquirer = require("inquirer");
+const fs = require("fs");
+const template = require("./src/template");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+
+// VARIABLES
+const employeeQuestions = [];
+
+const managerQuestions = [];
+
+const engineerQuestions = [];
+
+const internQuestions = [];
+
+const employeesArr = [];
+
+
+// FUNCTION DEFINITIONS
+
+// asks which kind of employee to add
+function addEmployee() {
+  // stub
+}
+
+// asks manager questions 
+function addManager() {
+  //stub
+}
+
+// asks engineer questions 
+function addEngineer() {
+  //stub
+}
+// asks intern questions 
+function addIntern() {
+  //stub
+}
+
+// generates HTML string and writes to /dist/index.html
+function generateHTML() {
+  //stub
+}
+
+// starts the app by asking for manager info
+function init() {
+  // stub
+}
+
+init();
