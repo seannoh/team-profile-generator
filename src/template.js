7a644c21777abba3e4a08708ca2e062c3f545fdc
Sean Oh <seanoh@ucsb.edu>
2022-07-19 10:47:43 -0700

    write template.js stubs

---
+++ src/template.js
@@ -0,0 +1,26 @@
+// HTML template for manager card
+function managerHTML(managerData) {
+  return ``;
+}
+
+// HTML template for engineer card
+function engineerHTML(engineerData) {
+  return ``;
+}
+
+// HTML template for intern card
+function internHTML(internData) {
+  return ``;
+}
+
+// Parses employee array and concatenates HTML cards
+function employeesHTML(employeesArr) {
+  return ``;
+}
+
+// HTMl template for full page
+function template(employeesArr) {
+  return ``;
+}
+
+module.exports = template;
