175e1e1985322f4cdb931e02f3a83120cc98826f
Sean Oh <seanoh@ucsb.edu>
2022-07-18 21:11:51 -0700

    add input validation tests to Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,6 +9,34 @@ describe("Employee", () => {
       expect(obj.id).toEqual(1);
       expect(obj.email).toEqual("example@example.com");
     });
+
+    it("should throw an error if name is not a non-empty string", () => {
+      const cb = () => new Employee(123, 1, "example@example.com");
+      const err = new Error("Expected 'name' to be a non-empty string");
+
+      expect(cb).toThrowError(err);
+    });
+
+    it("should throw an error if id is not a non-negative number", () => {
+      const cb = () => new Employee("John Doe", "1", "example@example.com");
+      const err = new Error("Expected 'id' to be a non-negative number");
+
+      expect(cb).toThrowError(err);
+    });
+
+    it("should throw an error if the email is not a non-empty string", () => {
+      const cb = () => new Employee("John Doe", 1, 123);
+      const err = new Error("Expected 'email' to be a non-empty string");
+
+      expect(cb).toThrowError(err);
+    });
+
+    it("should throw an error if the email is not valid", () => {
+      const cb = () => new Employee("John Doe", 1, "foobar");
+      const err = new Error("Expected 'email' to be a valid email address");
+
+      expect(cb).toThrowError(err);
+    });
   });
 
   describe("Accessors", () => {
